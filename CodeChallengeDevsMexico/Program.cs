a7601b657e7711d522b7f1407a3be6028f123765
Samuel Arellano <samuel.arellano@gmail.com>
2025-01-15 13:38:47 -0800

    Initial commit

---
+++ CodeChallengeDevsMexico/Program.cs
@@ -0,0 +1,25 @@
+﻿
+string input1 = "abc";
+string input2 = "abcdef";
+
+Console.WriteLine(FormatOutput(SplitCharacters(input1))); // Output: ["ab", "c_"]
+Console.WriteLine(FormatOutput(SplitCharacters(input2))); // Output: ["ab", "cd", "ef"]
+
+Console.ReadLine();
+
+static string[] SplitCharacters(string message)
+{
+    if (message.Length % 2 != 0)
+    {
+        message += "_";
+    }
+
+    return Enumerable.Range(0, message.Length / 2)
+                     .Select(i => message.Substring(i * 2, 2))
+                     .ToArray();
+}
+
+static string FormatOutput(string[] array)
+{
+    return $"[{string.Join(", ", array.Select(s => $"'{s}'"))}]";
+}
